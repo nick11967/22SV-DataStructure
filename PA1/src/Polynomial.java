a554225a4dd41286082f49ffd803b10ab615bb4a
YoonahPark <cufft@naver.com>
2022-07-08 16:31:35 +0900

    Change Polynomial.java

--- PA1/src/Polynomial.java
+++ PA1/src/Polynomial.java
@@ -1,41 +1,34 @@
 import java.util.*;
 
 public class Polynomial {
-  TreeMap<Integer, Integer> poly;
+  TreeMap<Integer, Integer> polyMap = new TreeMap<>(Comparator.reverseOrder());
   
   // Create an empty polynomial
-  public Polynomial() { poly = new TreeMap<>(Comparator.reverseOrder()); }
+  public Polynomial() { }
 
   // Create a single-term polynomial
-  public Polynomial(int coef, int exp) {
-    poly = new TreeMap<>(Comparator.reverseOrder());
-    poly.put(exp, coef);
-  }
+  public Polynomial(int coef, int exp) { polyMap.put(exp, coef); }
 
   // Add opnd to 'this' polynomial; 'this' is returned
   public Polynomial add(Polynomial opnd) {
-    TreeMap<Integer, Integer> otherPoly = opnd.poly;
-
-    for(int key: otherPoly.keySet()){
-      if(poly.containsKey(key))
-        poly.put(key, poly.get(key)+otherPoly.get(key));
+    for(int key: opnd.polyMap.keySet()){
+      if(polyMap.containsKey(key))
+        polyMap.put(key, polyMap.get(key)+opnd.polyMap.get(key));
       else
-        poly.put(key, otherPoly.get(key));
-      if(poly.get(key)==0) poly.remove(key);
+        polyMap.put(key, opnd.polyMap.get(key));
+      if(polyMap.get(key)==0) polyMap.remove(key);
     }
     return this;
   }
 
   // Subtract opnd from 'this' polynomial; 'this' is returned
   public Polynomial sub(Polynomial opnd) {
-    TreeMap<Integer, Integer> otherPoly = opnd.poly;
-
-    for(int key: otherPoly.keySet()){
-      if(poly.containsKey(key))
-        poly.put(key, poly.get(key)-otherPoly.get(key));
+    for(int key: opnd.polyMap.keySet()){
+      if(polyMap.containsKey(key))
+        polyMap.put(key, polyMap.get(key)+opnd.polyMap.get(key));
       else
-        poly.put(key, -otherPoly.get(key));
-      if(poly.get(key)==0) poly.remove(key);
+        polyMap.put(key, opnd.polyMap.get(key));
+      if(polyMap.get(key)==0) polyMap.remove(key);
     }
     return this;
   }
@@ -43,8 +36,8 @@ public class Polynomial {
   // Print the terms of 'this' polynomial in decreasing order of exponents.
   // No pair of terms can share the same exponent in the printout.
   public void print() {
-    for(int key: poly.keySet()){
-      System.out.print(poly.get(key)+" "+key+" ");
+    for(int key: polyMap.keySet()){
+      System.out.print(polyMap.get(key)+" "+key+" ");
     }
   }
 
