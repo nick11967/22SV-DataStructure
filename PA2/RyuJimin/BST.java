3d30370a1eb0a59a17e2d30cdec96723f273e290
nick11967 <nick11967@snu.ac.kr>
2022-07-17 23:43:47 +0900

    commit to make pa2 pull-request

---
+++ PA2/RyuJimin/BST.java
@@ -0,0 +1,18 @@
+// Bongki Moon (bkmoon@snu.ac.kr), Mar/27/2017
+
+public class BST { // Binary Search Tree implementation
+
+  public BST() { }
+
+  public void insert(String key) { }
+  public boolean find(String key) { }
+  public int size() { }
+
+  public int sumFreq() { }
+  public int sumProbes() { }
+  public void resetCounters() { }
+
+  public void print() { }
+
+}
+
---
+++ PA2/RyuJimin/LinearHash.java
@@ -0,0 +1,19 @@
+// In-memory adaptation of Linear Hashing for Strings.
+//
+// Bongki Moon (bkmoon@snu.ac.kr), Mar/27/2017
+
+import java.util.*;
+
+public class LinearHash {
+
+  public LinearHash(int HTinitSize)	// constructor for the Hash table
+
+  public int insertUnique(String word) {} // insert `word' to the Hash table.
+
+  public int lookup(String word) {}     // look up `word' in the Hash table.
+
+  public int wordCount() {}
+  public int emptyCount() {}
+  public int size() {}			// 2^k + collisions in the current round
+
+}
