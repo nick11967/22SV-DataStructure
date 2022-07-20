229e857ab114fa6fbac40fa03998a17cd8a960e5
nick11967 <nick11967@snu.ac.kr>
2022-07-20 17:56:01 +0900

    BST 22.07.20 17:56

--- PA2/RyuJimin/BST.java
+++ PA2/RyuJimin/BST.java
@@ -4,6 +4,15 @@ class Node {
   String key;
   int frequency = 1, access_count = 0;
   Node left, right;
+
+  void setKey(String key) {
+    this.key = key;
+  }
+
+  void resetFC() {
+    this.frequency = 1;
+    this.access_count = 0;
+  }
 }
 
 public class BST { // Binary Search Tree implementation
@@ -20,19 +29,21 @@ public class BST { // Binary Search Tree implementation
   }
 
   private Node insertItem(Node t, String key) { // 교재 p.351
-    Node r = new Node();
     if (t == null) {
-      r.key = new String(key);
+      Node r = new Node();
+      r.setKey(key);
       treeSize += 1;
       return r;
     } else if (key.compareTo(t.key) > 0) {
       t.left = insertItem(t.left, key);
+      return t;
     } else if (key.compareTo(t.key) < 0) {
       t.right = insertItem(t.right, key);
+      return t;
     } else {
       t.frequency += 1;
+      return t;
     }
-    return r;
   }
 
   public boolean find(String key) { // 교재 p.347
@@ -95,8 +106,7 @@ public class BST { // Binary Search Tree implementation
 
   private void resetCountersRC(Node t) {
     if (t != null) {
-      t.frequency = 1;
-      t.access_count = 0;
+      t.resetFC();
       resetCountersRC(t.left);
       resetCountersRC(t.right);
     }
